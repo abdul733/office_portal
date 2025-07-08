349cb3350a369b571af39abb4f08d7d0a68c8209
SHAIK ABDUL <umamahesh2936@gmail.com>
2025-07-08 17:13:30 +0530

    Test: GitHub connection check

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return "Hello, Flask! This is a GitHub test "
 
 if __name__ == '__main__':
     app.run(debug=True)
